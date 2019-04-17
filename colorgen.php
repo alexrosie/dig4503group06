34d7cccf987112145c8d7ad7d438c6b40e5e8b22
Alex <alexrosedecker@gmail.com>
2019-04-17 15:51:02 -0400

    fixing original element

--- colorgen.php
+++ colorgen.php
@@ -24,9 +24,11 @@
     </div>
     <br>
 <div class="container" style="width: 700px;">
-  <button class="btn btn-1" id="monobutton">Monochromatic</button>
-  <button class="btn btn-2" id="anabutton">Analogic</button>
-  <button class="btn btn-3" id="compbutton">Complementary</button>
+  <div class="container2">
+    <button class="btn btn-1" id="monobutton">Monochromatic</button>
+    <button class="btn btn-2" id="anabutton">Analogic</button>
+    <button class="btn btn-3" id="compbutton">Complementary</button>
+  </div>
   <div class="col" id="loadingCol">
     <!-- loading wheel -->
     <div class="d-flex justify-content-center">
